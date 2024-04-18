17a4ab43a401d03e37bec86c7f92f2f7c94aaa3b
cauematheus23 <cauematheus_23@hotmail.com>
2024-04-18 19:36:40 -0300

    program commit

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,11 @@
+﻿int x;
+Console.WriteLine("Digite o valor para verificar: ");
+x = int.Parse(Console.ReadLine());
+if ((x % 2) == 0)
+{
+    Console.WriteLine($"{x} é par");
+}
+else
+{
+    Console.WriteLine($"{x} é impar");
+}
